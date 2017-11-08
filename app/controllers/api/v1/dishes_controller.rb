f04cb7882423cc8e521d6fb31286c8033a3ca680
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-08 18:26:14 +0100

    finished refactoring according to PR comments

--- app/controllers/api/v1/dishes_controller.rb
+++ app/controllers/api/v1/dishes_controller.rb
@@ -1,14 +1,7 @@
 class Api::V1::DishesController < ApplicationController
-  before_action :set_default_response_format
 
   def index
-    @dishes = Dish.all
-    render json: @dishes
-  end
-
-  protected
-
-  def set_default_response_format
-    request.format = :json
+    dishes = Dish.all
+    render json: dishes
   end
 end
