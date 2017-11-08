2a6c6a380fecfe634eb8a3fa3f7682b136ff0423
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-08 18:24:48 +0100

    refactored according to PR comments

--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :dish do
     name "MyString"
     price 1
+    image 'image'
+    description 'description'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,8 +14,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.file_fixture_path = "spec/fixtures"
-
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
