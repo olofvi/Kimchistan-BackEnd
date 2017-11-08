fd3142cfd46f7c5dfa1284a483fd0875441daaa8
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-08 17:57:47 +0100

    refactoring according to PR comments Working on extracting expected response to file fixture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108150747) do
+ActiveRecord::Schema.define(version: 20171108164924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20171108150747) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
