0ce3c5afbe6077c74406fdf4ab2181e6fa50a339
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-08 16:38:43 +0100

    dishes endpoint sending json:api standard information

--- app/controllers/api/v1/dishes_controller.rb
+++ app/controllers/api/v1/dishes_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::DishesController < ApplicationController
+  before_action :set_default_response_format
+
   def index
     @dishes = Dish.all
+    render json: @dishes
+  end
+
+  protected
+
+  def set_default_response_format
+    request.format = :json
   end
 end
--- spec/requests/api/v1/dish_spec.rb
+++ spec/requests/api/v1/dish_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::DishesController, type: :request do
   describe 'GET /v1/dishes' do
+    let!(:dish) {FactoryBot.create(:dish, name: 'dumplings', price: 89)}
+
     it 'should return dishes' do
       get '/api/v1/dishes'
-      
+
       expect(response.status).to eq 200
+      expected_response = {"data"=>
+                               [{"id"=>"#{dish.id}",
+                                 "type"=>"dishes",
+                                 "attributes"=>
+                                     {"name"=>"dumplings",
+                                      "price"=>89}}]}
+      expect(response_json).to eq expected_response
     end
   end
 end
