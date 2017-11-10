16c6fd14f03723a81bcf1959c133025ca73114f9
olofvi <olofvi89@gmail.com>
2017-11-10 10:45:43 +0100

    rspec for restaurant model api factory for restaurant spec fixture file for spec generated restaurant model restaurant serializer routes for restaurant api

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :restaurant do
+    name 'Kimchistan'
+    address 'Hötorgsgången'
+    phonenumber 0766333552
+    hours '10-19'
+    description 'yummy korean food'
+  end
+end
---
+++ spec/requests/api/v1/restaurant_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ProductsController, type: :request do
+  describe 'GET /v1/restaurants' do
+    context 'return 1 restaurant' do
+      let!(:restaurant) {create(:restaurant, name: 'Kimchistan', address: 'Hötorgsgången', phonenumber:0766333552, hours:'10-19', description:'yummy korean food')}
+
+      it 'should return a restaurant' do
+        get '/api/v1/restaurants'
+
+        expect(response.status).to eq 200
+        expected_response = eval(file_fixture('restaurant.txt').read)
+        expect(response_json).to eq expected_response.as_json
+      end
+    end
+  end
+end
