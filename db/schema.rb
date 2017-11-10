9d66f2070ba821dfc8153c05b3e01ce39d6f51a2
olofvi <olofvi89@gmail.com>
2017-11-10 15:01:32 +0100

    dropped the db, created and migrated again

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171110125218) do
   create_table "ingredients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "available"
+    t.boolean "available", default: true
     t.integer "price"
     t.string "name"
   end
@@ -30,13 +30,6 @@ ActiveRecord::Schema.define(version: 20171110125218) do
     t.index ["product_id"], name: "index_ingredients_products_on_product_id"
   end
 
-  create_table "product_lines", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "product_id"
-    t.integer "ingredient_id"
-  end
-
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -44,8 +37,8 @@ ActiveRecord::Schema.define(version: 20171110125218) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "image"
-    t.string "of_type"
-    t.boolean "available"
+    t.string "of_type", default: "dish"
+    t.boolean "available", default: true
   end
 
   create_table "restaurants", force: :cascade do |t|
