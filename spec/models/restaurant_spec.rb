31f94f6de94b2ad513277721c8f335c9a2712afc
basil <basil.mawejje@gmail.com>
2017-11-10 12:12:39 +0200

    Add test to validate restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it {is_expected.to have_db_column :name}
+    it {is_expected.to have_db_column :address}
+    it {is_expected.to have_db_column :phonenumber}
+    it {is_expected.to have_db_column :hours}
+    it {is_expected.to have_db_column :description}
+  end
+
+  describe 'Validations' do
+    it {is_expected.to validate_presence_of :name}
+    it {is_expected.to validate_presence_of :address}
+    it {is_expected.to validate_presence_of :phonenumber}
+    it {is_expected.to validate_presence_of :hours}
+    it {is_expected.to validate_presence_of :description}
+  end
 end
