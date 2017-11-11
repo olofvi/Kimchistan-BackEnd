c202ae044cb1fb11c4cf68c6bacd2fdf073d746b
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-11 11:01:04 +0100

    working on the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env == 'development'
+  Product.destroy_all
+  Ingredient.destroy_all
+
+  ['Chicken', 'Tofu', 'Salmon'].each do |ingredient|
+    Ingredient.create(name: ingredient, price: 25 + rand(10), available: true)
+  end
+
+  ['Bibimbap', 'Funchöza', 'Hoe-deopbap', 'kimchi-jjigae', 'Korean tacos'].each do |dish|
+    prod = Product.create(name: dish,
+                             price: 89,
+                             description: 'Tasty food',
+                             image: 'http://img.taste.com.au/89rfrNZj/w720-h480-cfill-q80/taste/2016/11/bibimbap-92850-1.jpeg',
+                             of_type: 'dish',
+                             available: true)
+    prod.ingredients.push Ingredient.all
+  end
+
+  ['Herb Salad', 'Caesar Salad', 'Panzanella', 'Tricolor Salad', 'Yellow Trio'].each do |salad|
+    Product.create(name: salad,
+                   price: 55 + rand(20),
+                   description: 'Green salad',
+                   available: true,
+                   of_type: 'salad',
+                   image: 'https://goo.gl/images/HDFTq4')
+  end
+
+  ['Coca-Cola', 'Fanta', 'Pepsi', 'Loka', 'Green te', 'Black te', 'Mint te', 'Ginger beer', 'Juice', 'Saft'].each do |drink|
+    Product.create(name: drink,
+                   price: 10 + rand(10),
+                   available: true,
+                   of_type: 'drink',
+                   description: 'Beverage',
+                   image: 'https://goo.gl/images/Uh6t7A')
+  end
+end
