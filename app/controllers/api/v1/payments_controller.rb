aaf66d0c4e27af7bdb0bc010f2036ac885993427
olofvi <olofvi89@gmail.com>
2017-11-14 16:47:17 +0100

    stripe rspec 2 tests are green

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::PaymentsController < ApplicationController
+
   def create
     @amount = 500
-
+    
     customer = Stripe::Customer.create(
-        email: payment_params[:email],
-        source: payment_params[:token]
+        email: params[:payment][:data][:email],
+        source: params[:payment][:data][:token]
     )
 
     charge = Stripe::Charge.create(
@@ -20,6 +21,6 @@ class Api::V1::PaymentsController < ApplicationController
   private
 
   def payment_params
-    params[:data][:attributes]
+    params.permit[:data][:attributes]
   end
 end
--- spec/requests/api/v1/payment_spec.rb
+++ spec/requests/api/v1/payment_spec.rb
@@ -2,14 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::PaymentsController, type: :request do
   describe 'GET /v1/payments' do
+    before { StripeMock.start }
+    after { StripeMock.stop }
     context 'return 1 customer' do
 
-      StripeMock.start
-      customer = Stripe::Customer.create(
-          :email => 'me@me.com',
-          :card => 'valid_card_token'
-      )
-      end
+      # StripeMock.start
+      # let!(:customer) {Stripe::Customer.create(
+      #     :email => 'me@me.com',
+      #     :card => 'valid_card_token'
+      # )}
     end
   end
+
+  describe 'POST /api/v1/payments' do
+    let(:stripe_helper) { StripeMock.create_test_helper }
+    before { StripeMock.start }
+    after { StripeMock.stop }
+    let!(:customer) {Stripe::Customer.create(
+        :email => 'me@me.com',
+        :card => stripe_helper.generate_card_token
+    )}
+    it 'creates a data entry' do
+      post '/api/v1/payments', params: {
+          payment: {data: {email: 'me@me.com', token: customer.card}}
+      }
+
+      expect(response.status).to eq 200
+    end
+  end
+end
+
 
