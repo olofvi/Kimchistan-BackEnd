f18c76b3bdb3c23d4cf3fdaa8fb9461e1b56e456
BasilMawejje <basil.mawejje@gmail.com>
2017-11-15 16:27:30 +0200

    Order test green

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :order do
+    dish_name 'bibimbap'
+    dish_quantity 2
+    customer_name 'Thomas Ochman'
+    delivery false
     created_at '2017-11-10T09:34:11.608Z'
-    updated_at '2017-11-10T09:34:11.608Z'
   end
 end
--- spec/requests/api/v1/order_spec.rb
+++ spec/requests/api/v1/order_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::OrdersController, type: :request do
   describe 'GET /v1/orders' do
     context 'return 1 order' do
-      let!(:order) {create(:order, created_at: '2017-11-10T09:34:11.608Z', updated_at: '2017-11-10T09:34:11.608Z')}
+      let!(:order) {create(:order, dish_name: 'bibimbap', dish_quantity: 2, customer_name: 'Thomas Ochman', delivery: false, created_at: '2017-11-10T09:34:11.608Z')}
 
       it 'should return an order' do
         get '/api/v1/orders'
